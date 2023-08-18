2ec9fc1445dc798c11c666015c7a6936b42cccfe
Jolbert Sodré <55359499+sdrjolbert@users.noreply.github.com>
2023-08-18 15:48:07 -0300

    Update converter.js

--- converter.js
+++ converter.js
@@ -8,13 +8,13 @@ const keyboard = "529,99";
 const mousepad = "174,00";
 const monitor = "1.129,99";
 
-const pcCase = "419,00";
-const mobo = "2.319,99";
+const pcCase = "379,00";
+const mobo = "2.299,00";
 const cpu = "3.759,99";
-const wc = "699,90";
-const ram = "1.799,99";
+const wc = "849,00";
+const ram = "2.029,90";
 const psu = "1.999,99";
-const gpu = "9.997,00";
+const gpu = "7.989,99";
 const ssd = "1.199,99";
 
 const peripheral = [headset, mouse, keyboard, mousepad, monitor];
@@ -25,6 +25,7 @@ const pcValues = pc.map((e) => convertNumber(e));
 const totalPeripheral = peripheralValues.reduce((sum, value) => sum + value, 0);
 const totalPc = pcValues.reduce((sum, value) => sum + value, 0);
 const total = totalPeripheral + totalPc;
+const diferenca = (25 * 1000) - total;
 
 console.log("Todos os valores podem ser alterados simplesmente mudando os valores das variáveis acima!");
 
@@ -33,3 +34,4 @@ console.log("ALERTA!!! O valor de todos os totais não incluem o FRETE!!!");
 console.log(`Total dos Perfiféricos: ${totalPeripheral.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}`);
 console.log(`Total do PC: ${totalPc.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}`);
 console.log(`Total: ${total.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}`);
+console.log(`Difença do Orçamento (25K): ${diferenca.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}`);
